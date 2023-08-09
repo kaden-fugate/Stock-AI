76f7540f30bf32284aff702526d78e4f03e6cb15
kaden-ator <kkadenfugate@gmail.com>
2023-08-08 20:08:18 -0700

    Prepared data to be used in NN
    Imported data from yahoo, transformed the data into being usable for a NN to train on. Reshaped final transformed data to fit into LSTM model as it expects 3 dimensions and our training data is only 2 dimensional.

--- neural-network.py
+++ neural-network.py
@@ -12,15 +12,45 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM
 
 # Override pdr due to lack of connection between pdr and yahoo
-
 yfin.pdr_override() 
 
 # Store program vars
-
 STOCK_NAME = 'GOOG'
 START_DATE = '2000-01-01'
 END_DATE = '2023-07-01'
+PREDICTION_LEN = 60
 
-dataframe = pdr.get_data_yahoo(STOCK_NAME, start= '2023-02-01', end= '2023-02-02')
+# Load in selected stock over given start and end dates
+dataframe = pdr.get_data_yahoo(STOCK_NAME, start= START_DATE, end= END_DATE)
 
+# Store filtered closing price in dataset as a np array
+dataset = np.array( dataframe.filter(['Close']) )
 
+# Train on 80% of dataset
+training_len = math.ceil(len(dataset) * 0.8)
+
+# Normalize dataset for use in NN
+scale = MinMaxScaler(feature_range= (0, 1)) # Set range between 0 and 1
+scaled_data = scale.fit_transform(dataset)
+
+# Create data set to make training dataset for NN
+training_data = scaled_data[ :training_len, : ]
+
+prev_train = []
+next_train = []
+
+# Loop is training_len - PREDICTION_LEN long
+for day in range(PREDICTION_LEN, training_len):
+    
+    # hold a range of days PREDICTION_LEN long
+    prev_train.append( training_data[ day - PREDICTION_LEN : day, 0 ] )
+
+    # get next days closing price
+    next_train.append( training_data[ day, 0 ] )
+
+# LSTM model expects 3 dimensions
+# Need to reshape our training data to fit this model
+
+prev_train, next_train = np.array(prev_train), np.array(next_train)
+
+prev_train = np.reshape(prev_train, (prev_train.shape[0],prev_train.shape[1], 1))
