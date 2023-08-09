87f553801452c7a3b70b0fd91c769f0b08caed22
kaden-ator <kkadenfugate@gmail.com>
2023-08-09 10:12:40 -0700

    Built NN
    Built sequential NN with 2 layers of LSTM (100 nodes each) and 2 densely connected layers (50 and 1 node respectfully).

--- neural-network.py
+++ neural-network.py
@@ -50,7 +50,20 @@ for day in range(PREDICTION_LEN, training_len):
 
 # LSTM model expects 3 dimensions
 # Need to reshape our training data to fit this model
-
 prev_train, next_train = np.array(prev_train), np.array(next_train)
+prev_train = np.reshape(prev_train, (prev_train.shape[0], prev_train.shape[1], 1))
 
-prev_train = np.reshape(prev_train, (prev_train.shape[0],prev_train.shape[1], 1))
+# Now that we reshaped our data, we can build our LSTM model
+LSTM_model = Sequential()
+
+# Add LSTM layers of NN
+LSTM_model.add( LSTM(100, return_sequences= True, input_shape= (prev_train.shape[1], 1)) )
+LSTM_model.add( LSTM(100, return_sequences= False, ) )
+
+# Add regular densly connected layers of NN
+LSTM_model.add( Dense(50) )
+LSTM_model.add( Dense(1) )
+
+# Compile the model with the adam optimizer
+# Measure loss using MSQE
+LSTM_model.compile(optimizer= 'adam', loss= 'mean_squared_error')
