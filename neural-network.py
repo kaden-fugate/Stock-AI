6e33feb263c8c042222e065d13a22010f00286cb
kaden-fugate <kkadenfugate@gmail.com>
2023-08-10 13:49:48 -0700

    implemented future prediction
    Added ability for model to predict into future. It does this by predicting each day and using its previous predictions to make a new prediction. Due to over-fitting to the model, this feature is currently pretty inaccurate over 30 days into the future.

--- neural-network.py
+++ neural-network.py
@@ -10,15 +10,16 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+from keras.losses import Huber
 
 # Override pdr due to lack of connection between pdr and yahoo
 yfin.pdr_override() 
 
 # Store program vars
 STOCK_NAME = 'GOOG'
-START_DATE = '2000-01-01'
-END_DATE = '2033-07-01'
-PREDICTION_LEN = 4
+START_DATE = '1990-01-01'
+END_DATE = '2023-07-01'
+SLIDING_WINDOW_LEN = 45
 
 # Load in selected stock over given start and end dates
 dataframe = pdr.get_data_yahoo(STOCK_NAME, start= START_DATE, end= END_DATE)
@@ -39,11 +40,11 @@ training_data = scaled_data[ :training_len, : ]
 prev_train = []
 next_train = []
 
-# Loop is training_len - PREDICTION_LEN long
-for day in range(PREDICTION_LEN, training_len):
+# Loop is training_len - SLIDING_WINDOW_LEN long
+for day in range(SLIDING_WINDOW_LEN, training_len):
     
-    # hold a range of days PREDICTION_LEN long
-    prev_train.append( training_data[ day - PREDICTION_LEN : day, 0 ] )
+    # hold a range of days SLIDING_WINDOW_LEN long
+    prev_train.append( training_data[ day - SLIDING_WINDOW_LEN : day, 0 ] )
 
     # get next days closing price
     next_train.append( training_data[ day, 0 ] )
@@ -66,22 +67,22 @@ LSTM_model.add( Dense(1) )
 
 # Compile the model with the adam optimizer
 # Measure loss using MSQE
-LSTM_model.compile(optimizer= 'adam', loss= 'mean_squared_error')
+LSTM_model.compile(optimizer= 'adam', loss=Huber(delta=1.0))
 
 # Train the model
 LSTM_model.fit(prev_train, next_train, batch_size= 1, epochs= 2)
 
 # Make dataset for testing
-test_data = scaled_data[ training_len - PREDICTION_LEN : , : ]
+test_data = scaled_data[ training_len - SLIDING_WINDOW_LEN : , : ]
 
 # Make arrays for holding data to test model on (prev_test) 
 # and actual values the model is attempting to predict (actual_vals)
 prev_test = []
 actual_vals = dataset[ training_len : , : ]
 
-# Fill test dataset with last PREDICTION_LEN vals
-for day in range(PREDICTION_LEN, len(test_data)):
-    prev_test.append( test_data[day - PREDICTION_LEN : day, 0] )
+# Fill test dataset with last SLIDING_WINDOW_LEN vals
+for day in range(SLIDING_WINDOW_LEN, len(test_data)):
+    prev_test.append( test_data[day - SLIDING_WINDOW_LEN : day, 0] )
 
 # Convert to np array to be used in LSTM model
 prev_test = np.array(prev_test)
@@ -97,6 +98,27 @@ predicted = scale.inverse_transform(predicted)
 mean_squared_error = np.sqrt( np.mean( (predicted - actual_vals) ** 2 ) )
 print( "Root Mean Squared Error: " + str(mean_squared_error) )
 
+future_days = 50
+new_input = scale.transform(actual_vals)
+new_input = new_input[-SLIDING_WINDOW_LEN:]
+print("new_input:" + str(new_input))
+predicted_future = []
+
+for day in range(future_days):
+
+    # Reshape and normalize input
+    normalized_input = np.reshape(new_input, (1, SLIDING_WINDOW_LEN, 1))
+
+    future_price = LSTM_model.predict(normalized_input)
+    future_price_denorm = scale.inverse_transform(future_price)
+
+    # Append to list of future prices
+    predicted_future.append(future_price_denorm[0, 0])
+    
+    # Update new_input to include prediction
+    new_input = np.append(new_input[1:], future_price)
+
+print(predicted_future)
 # Data to be plotted
 data = dataframe.filter(['Close'])
 trained_on = data[ : training_len ]
@@ -111,6 +133,9 @@ plt.ylabel("Closing Price [USD]")
 plt.plot(dataframe.index[:training_len], trained_on, label='Trained On')
 plt.plot(dataframe.index[training_len:], actual, label='Actual')
 plt.plot(dataframe.index[training_len:], predicted, label='Predicted')
+
+future_dates = pd.date_range(start=dataframe.index[-1], periods=future_days + 1)[1:]
+plt.plot(future_dates, predicted_future, label='Predicted Future')
 
 plt.legend()
 plt.show()
